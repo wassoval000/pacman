10bf201b5238a90992367948e6dde7bed49a43b6
wassoval000 <wassoval000@warren.k12.in.us>
2018-04-18 11:16:22 -0400

    Initial commit

--- src/Board.java
+++ src/Board.java
@@ -12,7 +12,12 @@ public class Board extends JPanel implements ActionListener{
     static int rows = 700;
     static int col = 900;
     static int edge1 = 300;
-    static int edgeDist = 50;
+    static int e0 = 50;
+    static int e1 = 650;
+    static int e2 = 350;
+    static int mid0 = (edge1/2)+60;
+    static int mid1 = 450+(edge1/2);
+    static int mid2 = mid1+(edge1/3)+60;
 
     public static void setup(){
 
@@ -22,7 +27,24 @@ public class Board extends JPanel implements ActionListener{
             }
         }
         for(int i = 0; i < col-(edge1*2); i++){
-            arr[edgeDist][i] = 1;
+            arr[e0][i] = 1;
+            arr[e1][i] = 1;
+        }
+        for(int i = (edge1*2); i < col; i++){
+            arr[e0][i] = 1;
+            arr[e1][i] = 1;
+        }
+        for(int i = 0; i < (edge1/2.0); i++){
+            arr[e2][i] = 1;
+        }
+        for(int i = mid0; i < mid0+(edge1/3.0); i++){
+            arr[e2][i] = 1;
+        }
+        for(int i = mid1; i < mid1+(edge1/3.0); i++){
+            arr[e2][i] = 1;
+        }
+        for(int i = mid2; i < mid2+(edge1/3.0); i++){
+            arr[e2][i] = 1;
         }
 
     }
